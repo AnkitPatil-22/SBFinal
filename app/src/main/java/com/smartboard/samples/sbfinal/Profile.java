8d66b656c97bcc4e887cfe910c6643d09a10e6af
AnkitPatil-22 <ankitpatil2202@gmail.com>
2021-05-17 21:41:50 +0530

    SignIn intergatrion using Firebase.

---
+++ app/src/main/java/com/smartboard/samples/sbfinal/Profile.java
@@ -0,0 +1,41 @@
+package com.smartboard.samples.sbfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Profile extends AppCompatActivity {
+
+    TextView name, email;
+    Button logout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        logout = findViewById(R.id.logout);
+        name = findViewById(R.id.name);
+        email = findViewById(R.id.email);
+
+        GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(this);
+        if (signInAccount != null){
+            name.setText(signInAccount.getDisplayName());
+            name.setText(signInAccount.getEmail());
+        }
+
+        logout.setOnClickListener((view) -> {
+            FirebaseAuth.getInstance().signOut();
+            Intent intent = new Intent(getApplicationContext(), SignIn.class);
+            startActivity(intent);
+        });
+    }
+}
